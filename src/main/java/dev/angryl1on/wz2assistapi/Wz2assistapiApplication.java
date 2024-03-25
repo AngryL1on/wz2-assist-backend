f9c5f681800a2a7a938881a3a17fe51d5dcc2eea
AngryL1on <vadim.lushina@gmail.com>
2024-03-25 23:37:27 +0300

    Initial commit

---
+++ src/main/java/dev/angryl1on/wz2assistapi/Wz2assistapiApplication.java
@@ -0,0 +1,13 @@
+package dev.angryl1on.wz2assistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wz2assistapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wz2assistapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/angryl1on/wz2assistapi/Wz2assistapiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.angryl1on.wz2assistapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wz2assistapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
